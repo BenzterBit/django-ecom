36519fccae9b1546f6c1e4585c5a2c7331198c96
Arnab Bit <arnabbit1904@gmail.com>
2023-03-20 18:28:12 +0530

    end of video 7 1. Added homescreen component, that for now displays the available products which we get from the given resource products.js file 2. Passed products are props to Product.js and rendered products at runtime 3. Used the Card bootstrap component to create the product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-import { Container } from 'react-bootstrap'
-import Header from './components/Header';
-import './App.css';
-import Footer from './components/Footer';
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import "./App.css";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main className='py-6'><Container><h1>My app</h1></Container></main>
+
+      <main className="py-6">
+        <Container>
+          <HomeScreen/>
+          <h1>My app</h1>
+        </Container>
+      </main>
       <Footer />
     </div>
   );
