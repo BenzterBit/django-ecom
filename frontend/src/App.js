c99064a38dd783f0d70c5fe7d4b72f04b897b6a6
Arnab Bit <arnabbit1904@gmail.com>
2023-03-20 19:10:26 +0530

    end of video 9 1. Added BrowserRouter, Routes, Route to load specific components only when specific links are hit 2. Added Link to get rid of href and <a> tags, as react applications are single page, vs. <a> redirects you to a different page 3. Added LinkContainer which is like a Link tag but for bootstrap components 4. Added a new component for product screen, which will be developed in the next video

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,26 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./components/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
 
       <main className="py-6">
         <Container>
-          <HomeScreen/>
-          <h1>My app</h1>
+          <Routes>
+            <Route path="/" Component={HomeScreen} exact />
+            <Route path="/product/:id" Component={ProductScreen} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
