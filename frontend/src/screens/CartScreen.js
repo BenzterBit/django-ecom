6ce46f477809c71e937558f2dc798cc43d7154c2
Arnab Bit <arnabbit1904@gmail.com>
2023-03-30 15:10:38 +0530

    end of video 27 1. added functionality to retrieve the history, params, and location of the web page 2. retrieved the cart items from the store

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, Link, useNavigate, useLocation } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  ListGroupItem,
+  Form,
+} from "react-bootstrap";
+
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
 function CartScreen() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  let qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+  console.log("cartItems", cartItems);
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, id, qty]);
   return <div>CartScreen</div>;
 }
 
