23fa596c1272c955f92f1a6666558f1a962d1494
Arnab Bit <arnabbit1904@gmail.com>
2023-04-25 10:51:44 +0530

    sample message to be updated manually

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
+    path('users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('',views.getRoutes, name="routes"),
+    path('users/profile/',views.getUserProfile, name='users-profile'),
     path('products/',views.getProducts, name='all-products'),
     path('products/<str:pk>/',views.getProduct, name="unique-product"),
 ]
