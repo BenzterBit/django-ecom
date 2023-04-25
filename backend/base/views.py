7b07fba9b646e20bf72bec8e6e142e4dd5020a81
Arnab Bit <arnabbit1904@gmail.com>
2023-04-25 16:00:41 +0530

    end of video 35 1. added permission class to specific views only accessible to certain permission levels

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .models import Product
 from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
 from .products import products
+from django.contrib.auth.models import User
+
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -27,9 +30,17 @@ def getRoutes(request):
     return Response('Hello')
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user,many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users,many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
